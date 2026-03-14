aadfaf91e63e74f63a1e8afb68be659459c31333
zhangzhenhu <zhangzhenhu@enn.cn>
2026-03-14 12:34:22 +0800

    first commit

---
+++ llmctl.rb
@@ -0,0 +1,37 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+class Llmctl < Formula
+  desc "A CLI tool for testing and validating LLM services"
+  homepage "https://github.com/zhangzhenhu/llmctl"
+  license "MIT"
+  version "0.1.0"
+
+  # Use prebuilt binaries from GitHub Releases
+  if OS.mac?
+    if Hardware::CPU.arm?
+      url "https://github.com/zhangzhenhu/llmctl/releases/download/v#{version}/llmctl-#{version}-aarch64-apple-darwin.tar.gz"
+    else
+      url "https://github.com/zhangzhenhu/llmctl/releases/download/v#{version}/llmctl-#{version}-x86_64-apple-darwin.tar.gz"
+    end
+  elsif OS.linux?
+    if Hardware::CPU.arm?
+      url "https://github.com/zhangzhenhu/llmctl/releases/download/v#{version}/llmctl-#{version}-aarch64-unknown-linux-musl.tar.gz"
+    else
+      url "https://github.com/zhangzhenhu/llmctl/releases/download/v#{version}/llmctl-#{version}-x86_64-unknown-linux-musl.tar.gz"
+    end
+  end
+
+  depends_on "rust" => :build
+
+  def install
+    if File.exist?("llmctl")
+      bin.install "llmctl" => "llmctl"
+    else
+      system "cargo", "install", "--root", prefix, "--path", "."
+    end
+  end
+
+  test do
+    assert_match "llmctl", shell_output("#{bin}/llmctl --help")
+  end
+end
